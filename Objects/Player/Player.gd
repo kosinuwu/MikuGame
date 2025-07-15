5d2b2239f21b825950f09d057e42eb40f627ff1b
kosinuwu <duck512uwu@gmail.com>
2025-07-15 06:48:36 +0200

    test

--- Objects/Player/Player.gd
+++ Objects/Player/Player.gd
@@ -1,3 +1,10 @@
 class_name Player extends RigidBody2D
 
+const SPEED : int = 150
+const JUMP : int = -250
 
+func _process(delta : float) -> void:
+	var dir : int = Input.get_axis("LEFT", "RIGHT")
+	apply_force(Vector2(dir * SPEED, 0))
+	if Input.is_action_just_pressed("UP"):
+		linear_velocity.y = JUMP
